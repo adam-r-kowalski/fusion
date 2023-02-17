bb15db2c7959d34b244a3571e0339795ef197430
Adam Kowalski <adam.kowalski.work@gmail.com>
2023-02-17 12:54:18 -0800

    move wasm code to its own module

--- src/main.zig
+++ src/main.zig
@@ -1,8 +1,23 @@
 const std = @import("std");
 const fusion = @import("fusion");
 
-pub fn main() void {}
-
-test "run all tests" {
-	std.testing.refAllDecls(fusion);
+pub fn main() !void {
+	var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+	const allocator = arena.allocator();
+	const module = fusion.wasm.Module{
+        .funcs = &.{
+            .{
+                .name = "add",
+                .params = &.{ .{ .name = "lhs", .type = .i32 }, .{ .name = "rhs", .type = .i32 } },
+                .result = .i32,
+                .ops = &.{
+                    .{ .local_get = "lhs" },
+                    .{ .local_get = "rhs" },
+                    .i32_add,
+                },
+            },
+        },
+	};
+	const wat = try fusion.wasm.wat(allocator, module);
+	std.debug.print("{s}\n", .{wat});
 }
