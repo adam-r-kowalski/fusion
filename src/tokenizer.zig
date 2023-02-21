20c49bb93c636018f07e624817f4bd09a48300ee
Adam Kowalski <adam.kowalski.work@gmail.com>
2023-02-20 21:37:33 -0800

    tokenizer dsl

--- src/tokenizer.zig
+++ src/tokenizer.zig
@@ -31,12 +31,52 @@ pub const Token = struct {
     kind: Kind,
 };
 
-pub fn symbol(value: []const u8, start: [2]usize, end: [2]usize) Token {
+pub fn symbol(start: [2]usize, end: [2]usize, value: []const u8) Token {
     return .{
         .start = start,
         .end = end,
         .kind = .{ .symbol = value },
     };
+}
+
+pub fn int(start: [2]usize, end: [2]usize, value: []const u8) Token {
+    return .{
+        .start = start,
+        .end = end,
+        .kind = .{ .int = value },
+    };
+}
+
+pub fn float(start: [2]usize, end: [2]usize, value: []const u8) Token {
+    return .{
+        .start = start,
+        .end = end,
+        .kind = .{ .float = value },
+    };
+}
+
+pub fn left_bracket(start: [2]usize, end: [2]usize) Token {
+    return .{ .start = start, .end = end, .kind = .left_bracket };
+}
+
+pub fn left_brace(start: [2]usize, end: [2]usize) Token {
+    return .{ .start = start, .end = end, .kind = .left_brace };
+}
+
+pub fn left_paren(start: [2]usize, end: [2]usize) Token {
+    return .{ .start = start, .end = end, .kind = .left_paren };
+}
+
+pub fn right_bracket(start: [2]usize, end: [2]usize) Token {
+    return .{ .start = start, .end = end, .kind = .right_bracket };
+}
+
+pub fn right_brace(start: [2]usize, end: [2]usize) Token {
+    return .{ .start = start, .end = end, .kind = .right_brace };
+}
+
+pub fn right_paren(start: [2]usize, end: [2]usize) Token {
+    return .{ .start = start, .end = end, .kind = .right_paren };
 }
 
 fn trim(tokens: *Tokens) void {
@@ -51,7 +91,13 @@ fn trim(tokens: *Tokens) void {
     tokens.source = tokens.source[i..];
 }
 
+fn advance(tokens: *Tokens, columns: usize) void {
+    tokens.source = tokens.source[columns..];
+    tokens.pos[1] += columns;
+}
+
 fn tokenizeNumber(tokens: *Tokens) Token {
+    const start = tokens.pos;
     var i: usize = 1;
     var decimals: usize = if (tokens.source[0] == '.') 1 else 0;
     while (i < tokens.source.len) : (i += 1) {
@@ -62,25 +108,22 @@ fn tokenizeNumber(tokens: *Tokens) Token {
         }
     }
     const value = tokens.source[0..i];
-    tokens.source = tokens.source[i..];
-    if (value.len == 1) {
-        switch (value[0]) {
-            '-' => return .minus,
-            '.' => return .dot,
-            else => {},
-        }
-    }
-    const token: Token = if (decimals > 0) .{ .float = value } else .{ .int = value };
-    return token;
-}
-
-fn advance(tokens: *Tokens, columns: usize) void {
-    tokens.source = tokens.source[columns..];
-    tokens.pos[1] += columns;
+    advance(tokens, i);
+    // if (value.len == 1) {
+    //     switch (value[0]) {
+    //         '-' => return .minus,
+    //         '.' => return .dot,
+    //         else => {},
+    //     }
+    // }
+    return if (decimals > 0)
+        float(start, tokens.pos, value)
+    else
+        int(start, tokens.pos, value);
 }
 
 fn tokenizeSymbol(tokens: *Tokens) Token {
-    const begin = tokens.pos;
+    const start = tokens.pos;
     var i: usize = 0;
     while (i < tokens.source.len and tokens.source[i] != ' ') : (i += 1) {}
     const value = tokens.source[0..i];
@@ -88,12 +131,13 @@ fn tokenizeSymbol(tokens: *Tokens) Token {
     // if (std.mem.eql(u8, value, "not")) return .not;
     // if (std.mem.eql(u8, value, "and")) return .and_;
     // if (std.mem.eql(u8, value, "or")) return .or_;
-    return symbol(value, begin, tokens.pos);
+    return symbol(start, tokens.pos, value);
 }
 
 fn tokenizeOne(tokens: *Tokens, kind: Kind) Token {
-    tokens.source = tokens.source[1..];
-    return .{ .kind = kind, .start = .{ 0, 0 }, .end = .{ 0, 0 } };
+    const start = tokens.pos;
+    advance(tokens, 1);
+    return .{ .kind = kind, .start = start, .end = tokens.pos };
 }
 
 pub const Tokens = struct {
@@ -106,13 +150,13 @@ pub const Tokens = struct {
         trim(self);
         if (self.source.len == 0) return null;
         switch (self.source[0]) {
-            // '0'...'9', '-', '.' => return tokenizeNumber(self),
-            // '[' => return tokenizeOne(self, .left_bracket),
-            // ']' => return tokenizeOne(self, .right_bracket),
-            // '{' => return tokenizeOne(self, .left_brace),
-            // '}' => return tokenizeOne(self, .right_brace),
-            // '(' => return tokenizeOne(self, .left_paren),
-            // ')' => return tokenizeOne(self, .right_paren),
+            '0'...'9', '-', '.' => return tokenizeNumber(self),
+            '[' => return tokenizeOne(self, .left_bracket),
+            ']' => return tokenizeOne(self, .right_bracket),
+            '{' => return tokenizeOne(self, .left_brace),
+            '}' => return tokenizeOne(self, .right_brace),
+            '(' => return tokenizeOne(self, .left_paren),
+            ')' => return tokenizeOne(self, .right_paren),
             // '=' => return tokenizeOne(self, .equal),
             // '<' => return tokenizeOne(self, .less),
             // '>' => return tokenizeOne(self, .greater),
--- test/test_tokenizer.zig
+++ test/test_tokenizer.zig
@@ -4,6 +4,14 @@ const tokenize = fusion.tokenizer.tokenize;
 const tokenizeAlloc = fusion.tokenizer.tokenizeAlloc;
 const Token = fusion.tokenizer.Token;
 const symbol = fusion.tokenizer.symbol;
+const int = fusion.tokenizer.int;
+const float = fusion.tokenizer.float;
+const left_bracket = fusion.tokenizer.left_bracket;
+const left_brace = fusion.tokenizer.left_brace;
+const left_paren = fusion.tokenizer.left_paren;
+const right_paren = fusion.tokenizer.right_paren;
+const right_brace = fusion.tokenizer.right_brace;
+const right_bracket = fusion.tokenizer.right_bracket;
 
 fn expectEqualToken(expected: Token, actual: Token) !void {
     try std.testing.expectEqual(expected.start, actual.start);
@@ -29,11 +37,11 @@ test "symbols" {
     var actual = try tokenizeAlloc(source, allocator);
     defer allocator.free(actual);
     const expected: []const Token = &.{
-        symbol("text", .{ 0, 0 }, .{ 0, 4 }),
-        symbol("camelCase", .{ 0, 5 }, .{ 0, 14 }),
-        symbol("snake_case", .{ 0, 15 }, .{ 0, 25 }),
-        symbol("PascalCase", .{ 0, 26 }, .{ 0, 36 }),
-        symbol("😀", .{ 0, 37 }, .{ 0, 41 }),
+        symbol(.{ 0, 0 }, .{ 0, 4 }, "text"),
+        symbol(.{ 0, 5 }, .{ 0, 14 }, "camelCase"),
+        symbol(.{ 0, 15 }, .{ 0, 25 }, "snake_case"),
+        symbol(.{ 0, 26 }, .{ 0, 36 }, "PascalCase"),
+        symbol(.{ 0, 37 }, .{ 0, 41 }, "😀"),
     };
     try expectEqualTokens(expected, actual);
 }
@@ -44,16 +52,16 @@ test "numbers" {
     var actual = try tokenizeAlloc(source, allocator);
     defer allocator.free(actual);
     const expected: []const Token = &.{
-        .{ .int = "1" },
-        .{ .int = "42" },
-        .{ .int = "-9" },
-        .{ .int = "0" },
-        .{ .int = "-0" },
-        .{ .float = "3.14" },
-        .{ .float = ".25" },
-        .{ .float = "-.25" },
-        .{ .int = "1_000" },
-        .{ .float = "1_000." },
+        int(.{ 0, 0 }, .{ 0, 1 }, "1"),
+        int(.{ 0, 2 }, .{ 0, 4 }, "42"),
+        int(.{ 0, 5 }, .{ 0, 7 }, "-9"),
+        int(.{ 0, 8 }, .{ 0, 9 }, "0"),
+        int(.{ 0, 10 }, .{ 0, 12 }, "-0"),
+        float(.{ 0, 13 }, .{ 0, 17 }, "3.14"),
+        float(.{ 0, 18 }, .{ 0, 21 }, ".25"),
+        float(.{ 0, 22 }, .{ 0, 26 }, "-.25"),
+        int(.{ 0, 27 }, .{ 0, 32 }, "1_000"),
+        float(.{ 0, 33 }, .{ 0, 39 }, "1_000."),
     };
     try expectEqualTokens(expected, actual);
 }
@@ -64,12 +72,12 @@ test "braces brackets and parens" {
     var actual = try tokenizeAlloc(source, allocator);
     defer allocator.free(actual);
     const expected: []const Token = &.{
-        .left_bracket,
-        .left_brace,
-        .left_paren,
-        .right_paren,
-        .right_brace,
-        .right_bracket,
+        left_bracket(.{ 0, 0 }, .{ 0, 1 }),
+        left_brace(.{ 0, 1 }, .{ 0, 2 }),
+        left_paren(.{ 0, 2 }, .{ 0, 3 }),
+        right_paren(.{ 0, 3 }, .{ 0, 4 }),
+        right_brace(.{ 0, 4 }, .{ 0, 5 }),
+        right_bracket(.{ 0, 5 }, .{ 0, 6 }),
     };
     try expectEqualTokens(expected, actual);
 }
